42debff4c30c7e1d2f3c47026b13324ff3741872
jihwan629 <jihwan629@naver.com>
2021-05-05 20:37:58 +0900

    즐겨찾는 글 목록 화면 만들기

--- screens/BookmarkScreen.js
+++ screens/BookmarkScreen.js
@@ -5,11 +5,20 @@ import {
  } from 'react-native'
  import { SafeAreaView } from 'react-native-safe-area-context'
  import Header from '../components/Header'
+ import ArticleItem from '../components/ArticleItem'
 
 const BookmarkScreen = () => {
     return (
         <SafeAreaView style={styles.container}>
             <Header title="즐겨찾기"></Header>
+            <ArticleItem
+                article={{
+                    id: 1,
+                    title: '청춘의 문장들',
+                    content: '사이에 있는 것들, 쉽게 바뀌는 것들, 덧없이 사라지는 것들이 여전히 내 마음을 잡아끈다',
+                    date: '2019년 4월 7일',
+                }}
+            />
         </SafeAreaView>
         
     )
