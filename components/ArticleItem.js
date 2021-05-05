75aaadad8c9c1561c7b18792420bffb0e4d83c12
jihwan629 <jihwan629@naver.com>
2021-05-05 16:18:39 +0900

    글 정보 부품 만들기

---
+++ components/ArticleItem.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    Platform,
+} from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+
+const ArticleItem = ({
+    article: {
+        id,
+        title,
+        content,
+        date,
+    },
+}) => {
+    return (
+        <TouchableOpacity
+            activeOpacity={0.8}
+        >
+            <View style={styles.container}>
+                <View style={styles.icon}>
+                    <Ionicons name="md-list" size={14} color="#9e9e9e" />
+                </View>
+
+                <View style={styles.info}>
+                    <Text style={styles.title}>
+                        {title}
+                    </Text>
+                    <Text
+                        numberOfLines={2}
+                        style={styles.content}
+                    >
+                        {content}
+                    </Text>
+                    <Text style={styles.date}>
+                        {date}
+                    </Text>
+                </View>
+            </View>
+
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        padding: 16,
+        paddingBottom: 0,
+    },
+    icon: {
+        width: 16,
+        height: 16,
+        marginRight: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: Platform.select({
+            ios: 1,
+            android: 4,
+        })
+    },
+    info: {
+        flex: 1,
+        paddingBottom: 16,
+        borderBottomWidth: 1,
+        borderBottomColor: '#f1f1f1',
+    },
+    title: {
+        marginBottom: 8,
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#212121',
+    },
+    content: {
+        marginBottom: 4,
+        fontSize: 14,
+        color: '#9e9e9e',
+        lineHeight: 18,
+    },
+    date: {
+        fontSize: 12,
+        color: '#bdbdbd',
+    }
+})
+
+export default ArticleItem
--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -5,11 +5,28 @@ import {
  } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '../components/Header'
+import ArticleItem from '../components/ArticleItem'
 
 const ListScreen = () => {
     return (
         <SafeAreaView style={styles.container}>
             <Header title="나의글"></Header>
+            <ArticleItem
+                article={{
+                    id: 1,
+                    title: '청춘의 문장들',
+                    content: '사이에 있는 것들, 쉽게 바뀌는 것들, 덧없이 사라지는 것들이 여전히 내 마음을 잡아끈다',
+                    date: '2019년 4월 7일',
+                }}
+            />
+            <ArticleItem
+                article={{
+                    id: 2,
+                    title: '문장들',
+                    content: '내게도 꿈이라는 것이 몇 개 있다. 그 중 하나는 마음을 잡아끄는 그 절실함을 문장으로 옮기는 일.',
+                    date: '2019년 4월 7일',
+                }}
+            />
         </SafeAreaView>
         
     )
