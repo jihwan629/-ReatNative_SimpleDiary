2baff8a207b706aa91b28f781f74128801326613
jihwan629 <jihwan629@naver.com>
2021-05-05 20:30:22 +0900

    글 내용 표시하기

--- screens/ViewScreen.js
+++ screens/ViewScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { 
     Text,
+    ScrollView,
     StyleSheet,
  } from 'react-native'
  import { SafeAreaView } from 'react-native-safe-area-context'
@@ -10,6 +11,15 @@ import {
      return (
          <SafeAreaView style={styles.container}>
              <ViewHeader />
+             <ScrollView>
+                 <Text style={styles.content}>
+                    오늘의 넷플릭스에서 나의 문어 선생님을 봤다. 정말 재밌고 슬펐다.
+                    다음에는 타이거 킹을 볼 생각이다.
+                 </Text>
+                 <Text style={styles.date}>
+                     2021년 5월 5일
+                 </Text>
+             </ScrollView>
          </SafeAreaView>
      )
  }
@@ -17,6 +27,18 @@ import {
  const styles = StyleSheet.create({
      container: {
          flex: 1,
+     },
+     content: {
+         padding: 20,
+         fontSize: 16,
+         lineHeight: 20,
+         color: '#424242',
+     },
+     date: {
+         padding: 20,
+         paddingTop: 20,
+         fontSize: 12,
+         color: '#bdbdbd',
      }
  })
 
