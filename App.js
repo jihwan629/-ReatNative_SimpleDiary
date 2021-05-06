7f3d6b7dee1e5a43e9d35d941d5f951933238c23
jihwan629 <jihwan629@naver.com>
2021-05-06 21:26:42 +0900

    정보 저장하고 불러오기

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
 import React from 'react'
+import AsyncStroage from '@react-native-async-storage/async-storage'
 import { ContextProvider } from 'react-simplified-context'
 import Navigator from './Navigator'
 
 export default class App extends React.Component {
   state = {
-    articles: [{
-      id: 1,
-      title: '감정',
-      content: `사람은 단어에서 감정을 배워. 단어가 없으면 사람은 자신의 감정을 훨씬 뭉뚱그려서 표현할 수 밖에 없어. 고독이라는 단어가 없다면 쓸쓸하다라고 생각 할 수 밖에 없고, 쓸쓸함이란 단어가 없으면 외롭다는 감정만 느낄 수 있어. 귀여운 것, 이쁜 것, 웅장한 것, 멋진 것을 그저 ‘아름답다’ 만으로 표현하는건 고통이야.`,
-      date: '2019년 4월 5일',
-      bookmarked: true,
-    }, {
-      id: 2,
-      title: '감정2',
-      content: `반대로 단어를 알게 되면 사람은 단어에 감정이 귀속돼. 단어에 자기 감정을 맞추는거지. 옷 사이즈처럼 가장 근접한 단어에 감정을 집어 넣는거야.완벽히 맞진 않지만 대충은 맞으니까. 편하니까. 그 단어가 자기 감정이라 착각하는거지.`,
-      date: '2019년 4월 5일',
-      bookmarked: false,
-    }],
-    id: 3,
+    articles: [],
+    id: 0,
+  }
+
+  componentDidMount() {
+    AsyncStroage.getItem('@diary:state').then((state) => {
+      this.setState(JSON.parse(state))
+    })
+  }
+
+  save = () => {
+    AsyncStroage.setItem('@diary:state', JSON.stringify(this.state)) 
   }
 
   render() {
@@ -38,7 +37,7 @@ export default class App extends React.Component {
               date: `${now.getFullYear()}년 ${now.getMonth() + 1}월 ${now.getDate()}일`
             }].concat(this.state.articles),
             id: this.state.id + 1,
-          })
+          }, this.save)
           
         }}
 
@@ -53,7 +52,7 @@ export default class App extends React.Component {
 
           this.setState({
             articles: newArticles,
-          })
+          }, this.save)
         }}
 
         toggle={(id) => {
@@ -66,7 +65,7 @@ export default class App extends React.Component {
 
           this.setState({
             articles: newArticles,
-          })
+          }, this.save)
         }}
       >
         <Navigator />
